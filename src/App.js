4c9633f4d587252ffdc657a8c76f2111143b9e1f
Alex Rimlin <alexander.rimlin@protonmail.com>
2021-01-15 10:48:47 +0200

    Last fixing commit (hopefully)

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          DO IT NOW!!!
+          Learn React
         </a>
       </header>
     </div>
