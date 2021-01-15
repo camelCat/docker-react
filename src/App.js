be5171b4e0f287c77a648bcafb82382d939c0679
Alex Rimlin <alexander.rimlin@protonmail.com>
2021-01-15 10:35:27 +0200

    Yet another fix (in App.js)

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
         <p>Bye there!</p>
-=======
-        <p>
-          Change your <code>gender</code> and chainsaw your family.
-        </p>
->>>>>>> 6cac22fac82657d1ed53749a420c03c62cf9928f
         <a
           className="App-link"
           href="https://reactjs.org"
